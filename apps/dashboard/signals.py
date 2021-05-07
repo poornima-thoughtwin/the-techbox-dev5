713c8d1e9634a876b9d938fe84a93d255980b6b5
thoughtwin <poornima.thoughtwin@gmail.com>
2021-05-07 13:34:27 +0530

    .

--- apps/dashboard/signals.py
+++ apps/dashboard/signals.py
@@ -6,17 +6,17 @@ from django.core.mail import send_mail
 from django.conf import settings
 from apps.dashboard.task import asset_create_mail
 
-@receiver(post_save, sender=Asset) 
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        asset_create_mail()
+# @receiver(post_save, sender=Asset) 
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         asset_create_mail()
      
 
 
-@receiver(post_save, sender=AssetAssign) 
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        asset_create_mail()
+# @receiver(post_save, sender=AssetAssign) 
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         asset_create_mail()
      
 
 #......................................................................................
