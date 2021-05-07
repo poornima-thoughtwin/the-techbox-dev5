c545ef3f757f86ea8df876658c51d4b911adeb53
thoughtwin <poornima.thoughtwin@gmail.com>
2021-05-07 13:16:49 +0530

    admin

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -2,17 +2,45 @@ from django.contrib import admin
 from .models import Employee,Designation,Asset,Category,AssetAssign
 
 # Register your models here.
-admin.site.register(Employee)
-admin.site.register(Designation)
-admin.site.register(Asset)
-admin.site.register(Category)
-admin.site.register(AssetAssign)
+# admin.site.register(Employee)
+# admin.site.register(Designation)
+# admin.site.register(Asset)
+# admin.site.register(Category)
+# admin.site.register(AssetAssign)
 
+@admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-	def Action(self, obj):
-		return format_html('')
 	search_fields=['name','designation','status']
 	list_display = ['name', 'email', 'designation', 'phone', 'address','status']
 	list_display_links = None
 	list_editable = ['status']
 	list_per_page = 10
+
+@admin.register(Designation)
+class DesignationAdmin(admin.ModelAdmin):
+	search_fields=['name']
+	list_display = ['name']
+	
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+	search_fields=['name','category','model_number','expire_date','availability']
+	list_display =['name','category','model_number','expire_date','availability']
+	list_display_links = None
+	list_editable = ['availability']
+	list_per_page = 10
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	search_fields=['name']
+	list_display = ['name']
+	
+
+@admin.register(AssetAssign)
+class AssetAssignAdmin(admin.ModelAdmin):
+	search_fields=['employee','asset','expire_date']
+	list_display =['employee','asset','expire_date']
+	# list_display_links = None
+	# list_editable = ['expire_date']
+	# list_per_page = 10
